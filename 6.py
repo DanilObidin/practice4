db6d934e6e56ad99c1f24d6d058eae055d9da055
DanilObidin <160224937+DanilObidin@users.noreply.github.com>
2024-03-07 17:29:53 +0700

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,7 @@
+a, b = input().split(":")
+if a == b:
+    print(0)
+if a > b:
+    print(1)
+if a < b:
+    print(2)
---
+++ 7.py
@@ -0,0 +1,7 @@
+k, a, s = map(int, input().split())
+if k > a and k > s:
+    print(k)
+if a > k and a > s:
+    print(a)
+if s > k and s > a:
+    print(s)
